9429f964aa8a316b67933c37856281c54ae1909f
Ryan Tablada <ryan.tablada@gmail.com>
2016-03-10 21:26:15 -0600

    style: editor

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -6,7 +6,7 @@ module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
     codemirror: {
       modes: ['sql'],
-      themes: ['solarized'],
+      themes: ['lesser-dark'],
     }
   });
 
